66fbc7b436ee405361a0ecb79c754be9b9f05b13
Ahmed Abdellatif <s270144@studenti.polito.it>
2023-07-19 12:18:55 +0200

    Linear Search & requirements

--- main.c
+++ main.c
@@ -4,24 +4,45 @@
 #include <string.h>
 #include <unistd.h>
 
+#define MAX 1e6
+
+bool check(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        // Check if the current element is greater than the next element
+        if (arr[i] > arr[i + 1])
+            return false;
+    }
+    return true;
+}
+
 bool exists(int ints[], int count, int k)
 {
-    /* Linear search */
-    int idx;
-    for (int i = 0; i < count; i++)
+    // handlers
+    bool isValid = (ints != NULL);
+    bool isBound = (count < MAX + 1);
+    bool isArranged = check(ints, count);
+
+    if (isValid && isBound && isArranged)
     {
-        if (ints[i] == k)
+        /* Linear search */
+        for (int i = 0; i < count; i++)
         {
-            idx = i;
-            return true; // target found
+            if (ints[i] == k)
+            {
+
+                return true; // target found
+            }
         }
     }
+
     return false;
 }
 
 int main()
 {
-    int ints[] = {-9, 14, 37, 102};
+    int ints[] = {1, 14, 37, 102};
     int ret = exists(ints, 4, 102); // 1
     ret = exists(ints, 4, 36);      // 0
     return 0;
